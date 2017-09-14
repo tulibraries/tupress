ff9fbccbb54367863bf18c6241bf3b06c359a7ef
Chad Nelson <chad.nelson@temple.edu>
2017-09-14 13:12:35 -0400

    Add relative root url config

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,12 @@ module Tupress
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    begin
+      # Tell rails the applicaiton will be served from a subdirectory.
+      config.relative_url_root = config_for(:deploy_to)['path']
+    rescue
+    # Do nothing and expect the application to be server in root path.
+    end
   end
 end
