36a00dfef9eb97ed73544787e9634842baa04e9e
Chris Doyle <cdoyle@temple.edu>
2017-10-31 12:53:04 -0400

    Added epub viewer epub.js with views for index and show.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809195122) do
+ActiveRecord::Schema.define(version: 20171024162028) do
 
   create_table "agencies", force: :cascade do |t|
     t.string   "title"
@@ -150,6 +150,18 @@ ActiveRecord::Schema.define(version: 20170809195122) do
     t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "oabooks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "isbn"
+    t.string   "cover_image_file_name"
+    t.string   "cover_image_content_type"
+    t.integer  "cover_image_file_size"
+    t.datetime "cover_image_updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "pages", force: :cascade do |t|
