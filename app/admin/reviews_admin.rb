7f86f4ca0e18ef1396a6ecf423a0da790a039fa5
Chris Doyle <cdoyle@temple.edu>
2018-02-16 11:23:12 -0500

    Added search gem to Trestle.

--- app/admin/reviews_admin.rb
+++ app/admin/reviews_admin.rb
@@ -7,6 +7,10 @@ Trestle.resource(:reviews) do
     Review.order(title: :asc).order(weight: :desc)
   end
 
+  search do |q|
+    collection.where("title LIKE ?", "%#{q}%")
+  end
+
   # Customize the table columns shown on the index view.
   #
   table do
@@ -23,7 +27,7 @@ Trestle.resource(:reviews) do
     # text_field    :title_id, disabled: true
     text_field    :author, disabled: true
     editor        :review
-    number_field  :weight, id: "review-weight", min: 0, max: 4
+    number_field  :weight, id: "review-weight", min: 0, max: 99
   
   #   row do
   #     col(xs: 6) { datetime_field :updated_at }
