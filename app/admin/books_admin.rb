d1e64f06eb0d422ea3a28cf2741e6c3b76740ea0
Chris Doyle <cdoyle@temple.edu>
2018-02-20 18:18:21 -0500

    Reintroduced side bar elements for books admin page.

--- app/admin/books_admin.rb
+++ app/admin/books_admin.rb
@@ -52,25 +52,25 @@ Trestle.resource(:books) do
     #   col(xs: 6) { datetime_field :created_at, disabled: true }
     # end
 
-    # if !book.cover_image.nil?
-    #   sidebar do 
-    #     label "cover_image"
-    #   end
-    #   sidebar do 
-    #     image_tag book.cover_image.url.to_s, id: "cover_image"
-    #   end
-    # end
-    # if !book.highlight_image.nil?
-    #   sidebar do 
-    #     label "highlight_image"
-    #   end
-    #   sidebar do 
-    #     image_tag book.highlight_image.url.to_s, id: "highlight_image"
-    #   end
-    # end
-    # sidebar do 
-    #     link_to "View Book Page", "/book/"+book.book_id, :class=>"view-book"
-    # end
+    if !book.cover_image.nil?
+      sidebar do 
+        label "cover_image"
+      end
+      sidebar do 
+        image_tag book.cover_image.url.to_s, id: "cover_image"
+      end
+    end
+    if !book.highlight_image.nil?
+      sidebar do 
+        label "highlight_image"
+      end
+      sidebar do 
+        image_tag book.highlight_image.url.to_s, id: "highlight_image"
+      end
+    end
+    sidebar do 
+        link_to "View Book Page", "/book/"+book.book_id, :class=>"view-book"
+    end
   end
 
   # By default, all parameters passed to the update and create actions will be
