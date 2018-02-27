5143662d1c6ec8decbe109aa6ad1fee48e00b618
Chris Doyle <cdoyle@temple.edu>
2018-02-27 09:22:47 -0500

    corrected book admin page.

--- app/admin/books_admin.rb
+++ app/admin/books_admin.rb
@@ -52,7 +52,7 @@ Trestle.resource(:books) do
     text_area     :news_text
     check_box     :highlight
     file_field    :highlight_image
-    check_box     :remove_image
+    check_box     :remove_cover_image
 
     # row do
     #   col(xs: 6) { datetime_field :updated_at, disabled: true }
