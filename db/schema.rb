e9d9319cb01b9f80bded495764c82546ee0e39f7
Chris Doyle <cdoyle@temple.edu>
2018-03-02 13:53:50 -0500

    Added catalog backend.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226204135) do
+ActiveRecord::Schema.define(version: 20180302181422) do
 
   create_table "administrators", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20180226204135) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "code"
   end
 
   create_table "conferences", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
