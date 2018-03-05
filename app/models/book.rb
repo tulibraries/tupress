b360d5034a8673d70f0c042a767b5e37e0a52e75
Chris Doyle <cdoyle@temple.edu>
2018-03-05 11:56:37 -0500

    Re-enabled the book cover image uploader after running book import task on production.

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,7 +14,7 @@ require 'carrierwave/orm/activerecord'
 	belongs_to :season, :counter_cache => true, optional: true
 	has_many :reviews
 
-	# mount_uploader :cover_image, BookCoverUploader
+	mount_uploader :cover_image, BookCoverUploader
 	mount_uploader :highlight_image, HighlightImageUploader
 
 	def self.search(q)
