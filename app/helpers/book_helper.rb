bb4188eb0c7185c8a44f8cf2fedcb43b8aecedf9
Chris Doyle <cdoyle@temple.edu>
2018-04-10 11:04:31 -0400

    Refined awards by year to checl all year fields.

--- app/helpers/book_helper.rb
+++ app/helpers/book_helper.rb
@@ -18,6 +18,10 @@ module BookHelper
     @subject_size = @books.where('subjects LIKE ?', "%subject_id: '"+subject_id+"'%").size 
   end
   def award_year_count(award_year)
-    @year_size = @books.where('award_year = ?', award_year).size
+    @year_size1 = @books.where('award_year = ?', award_year).size
+    @year_size2 = @books.where('award_year2 = ?', award_year).size
+    @year_size3 = @books.where('award_year3 = ?', award_year).size
+    @year_size4 = @books.where('award_year4 = ?', award_year).size
+    @year_size = @year_size1 + @year_size2 + @year_size3 + @year_size4
   end
 end
