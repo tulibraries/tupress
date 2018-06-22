8c07b624a0ef3ec37d5676a0e5a4e1768c659e8e
Chris Doyle <cdoyle@temple.edu>
2018-06-22 19:32:54 -0400

    Remvoed regex from sal queries for searches

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,7 +4,8 @@ class Page < ApplicationRecord
   
   def self.search(q)
 	  if q
-			@site = Page.where('name REGEXP ? AND id != 10 AND id != 11 AND id != 12', "(^|\\W)#{q}(\\W|$)").or(Page.where('content REGEXP ? AND id != 11 AND id != 12', "(^|\\W)#{q}(\\W|$)"))
+			# @site = Page.where('name REGEXP ? AND id != 10 AND id != 11 AND id != 12', "(^|\\W)#{q}(\\W|$)").or(Page.where('content REGEXP ? AND id != 11 AND id != 12', "(^|\\W)#{q}(\\W|$)"))
+			@site = Page.where('name LIKE ? AND id != 10 AND id != 11 AND id != 12', "%#{q}%").or(Page.where('content LIKE ? AND id != 11 AND id != 12', "%#{q}%"))
 		end
 	end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,8 @@ class Person < ApplicationRecord
 	# belongs_to :document
 		def self.search(q)
 	  if q
-	    @people = (Person.where('name REGEXP ?', "(^|\\W)#{q}(\\W|$)")).or(Person.where('position REGEXP ?', "(^|\\W)#{q}(\\W|$)"))
+	    # @people = (Person.where('name REGEXP ?', "(^|\\W)#{q}(\\W|$)")).or(Person.where('position REGEXP ?', "(^|\\W)#{q}(\\W|$)"))
+	    @people = Person.where('name LIKE ?', "#{q}").or(Person.where('position LIKE ?', "%#{q}%"))
 		end
 	end
 end
