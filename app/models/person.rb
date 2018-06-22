3a8a6459b1c5dd85a075e6655c5b0de7272910b0
Chris Doyle <cdoyle@temple.edu>
2018-06-22 19:35:50 -0400

    Missed wildcard operator

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,7 +3,7 @@ class Person < ApplicationRecord
 		def self.search(q)
 	  if q
 	    # @people = (Person.where('name REGEXP ?', "(^|\\W)#{q}(\\W|$)")).or(Person.where('position REGEXP ?', "(^|\\W)#{q}(\\W|$)"))
-	    @people = Person.where('name LIKE ?', "#{q}").or(Person.where('position LIKE ?', "%#{q}%"))
+	    @people = Person.where('name LIKE ?', "%#{q}%").or(Person.where('position LIKE ?', "%#{q}%"))
 		end
 	end
 end
