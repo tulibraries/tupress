484fe58855c237cb688451981bb1c462642394c6
Chris Doyle <cdoyle@temple.edu>
2018-06-22 20:04:11 -0400

    Added conferences to the blank check on search page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-	Rails.logger = Logger.new(STDOUT)
+	# Rails.logger = Logger.new(STDOUT)
 
   # rescue_from Exception do |error|
   # 	logger.info "Redirect to homepage due to error: #{error}"
