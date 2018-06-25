ee265820ee9a2736a3fd017a2d96f94109f0a5c1
Chris Doyle <cdoyle@temple.edu>
2018-06-25 11:07:42 -0400

    Double quote test

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,8 +1,8 @@
 class Faq < ApplicationRecord
 		def self.search(q)
 	  if q
-	    # @faqs = Faq.where('q REGEXP ?', "(^|\\W)#{q}(\\W|$)").or(Faq.where('a REGEXP ?', "(^|\\W)#{q}(\\W|$)")).order(:q)
-	    @faqs = Faq.where("q LIKE ?", "#{q}").or(Faq.where("a LIKE ?", "%#{q}%")).order(:q)
+	    @faqs = Faq.where("q REGEXP ?", "(^|\\W)#{q}(\\W|$)").or(Faq.where("a REGEXP ?", "(^|\\W)#{q}(\\W|$)")).order(:q)
+	    # @faqs = Faq.where("q LIKE ?", "#{q}").or(Faq.where("a LIKE ?", "%#{q}%")).order(:q)
 		end
 	end
 end
