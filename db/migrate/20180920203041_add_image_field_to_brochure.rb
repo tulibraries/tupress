ddc00856cec2093fc031d3a81ed8f1337de48019
= <=>
2018-09-21 16:11:48 -0400

    Cleaned up migrations.

--- db/migrate/20180920203041_add_image_field_to_brochure.rb
+++
@@ -1,5 +0,0 @@
-class AddImageFieldToBrochure < ActiveRecord::Migration[5.0]
-  def change
-    add_column :brochures, :image, :string
-  end
-end
--- db/migrate/20180921123010_create_brochures.rb
+++ db/migrate/20180921123010_create_brochures.rb
@@ -3,6 +3,10 @@ class CreateBrochures < ActiveRecord::Migration[5.0]
     create_table :brochures do |t|
       t.string :title
       t.string :subject_id
+      t.string :pdf
+      t.string :catalog_code
+      t.string :image
+
       t.boolean :promoted_to_subject
       t.boolean :promoted_to_homepage
 
